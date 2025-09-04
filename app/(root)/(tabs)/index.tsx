5c70eeccf21cdcf3bc0d0b7c3fe58cf1dd7d3343
Jason <byronjason902@gmail.com>
2025-09-04 15:21:04 +0100

    feat: add nativewind styling

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg">Welcome to Restate</Text>
+      <Link href="/sign_in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Explore</Link>
+      <Link href="/properties/1">property</Link>
+    </View>
+  );
+}
