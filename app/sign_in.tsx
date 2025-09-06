d8c58f44c4f372e8b8ce8e4a21153e8927b6a5c6
Jason <byronjason902@gmail.com>
2025-09-06 12:01:05 +0100

    Update: Sign_In page

--- app/sign_in.tsx
+++ app/sign_in.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import React from "react";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+images;
 
 const sign_in = () => {
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
 
-export default sign_in
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To Real Scout
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-primary-300 text-center mt-1">
+            Your Ideal Home
+          </Text>
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Real Scout with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-3 mt-5 flex-row items-center justify-center"
+            activeOpacity={0.8}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Image
+              source={icons.google}
+              className="w-5 h-5 mr-3"
+              resizeMode="contain"
+              style={{ width: 20, height: 20, marginRight: 12 }}
+            />
+            <Text className="text-lg font-rubik-medium text-black-300">
+              Continue with Google
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default sign_in;
